b657eb7f1d071ad9f787645f8cff2ad65183e3b7
Rahul naik <rahulnaik12june@gmail.com>
2025-12-05 21:54:51 +0530

    added files

--- WebApplicationMVC/obj/Debug/net8.0/WebApplicationMVC.AssemblyInfo.cs
+++ WebApplicationMVC/obj/Debug/net8.0/WebApplicationMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb45a24c4fe256107f6357438a8415c4d55af75")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
